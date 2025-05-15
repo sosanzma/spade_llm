42d141394344b971b2d096486d1209fee623d4e7
manel <manelbng@gmail.com>
2025-05-15 10:02:52 +0200

    innit update

--- spade_llm/__init__.py
+++ spade_llm/__init__.py
@@ -6,7 +6,7 @@ from .behaviour import LLMBehaviour
 from .context import ContextManager
 from .tools import LLMTool
 from .agent import LLMAgent
-from .providers import LLMProvider
+from .providers import LLMProvider, OpenAILLMProvider
 from .utils import load_env_vars
 from .routing import RoutingFunction, RoutingResponse
 
@@ -16,6 +16,7 @@ __all__ = [
     "LLMTool", 
     "LLMAgent", 
     "LLMProvider",
+    "OpenAILLMProvider",
     "load_env_vars",
     "RoutingFunction",
     "RoutingResponse",
--- spade_llm/providers/__init__.py
+++ spade_llm/providers/__init__.py
@@ -1,6 +1,6 @@
 """LLM providers for SPADE-LLM."""
 
 from .base_provider import LLMProvider
+from .open_ai_provider import OpenAILLMProvider
 
-
-__all__ = ["LLMProvider"]
+__all__ = ["LLMProvider", "OpenAILLMProvider"]
